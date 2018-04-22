00a37e91a4cae0e8bf7229e43264772537aa89b3
vagrant <vagrant@homestead>
2018-04-22 18:56:40 +0000

    added sales controller cron job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Http\Controllers\SaleController;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +25,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call('App\Http\Controllers\SaleController@store')
+        ->everyFiveMinutes();
     }
 
     /**
