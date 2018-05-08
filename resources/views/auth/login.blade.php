ecc60c0023d6648a3205dbeb660545203e7931cb
vagrant <vagrant@homestead>
2018-05-08 22:54:03 +0000

    created guest layout, added login.blade.php front

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,76 +1,77 @@
-@extends('layouts.app')
+@extends('layouts.guest')
 
 @section('head')
 <script src='https://www.google.com/recaptcha/api.js'></script>
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }} {{ $fail_counter }}</div>
+<main class="">
+    <div class="canvas">
+        <div class="bubble-1"></div>
+        <div class="shape-1"></div>
+    </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}" autocomplete="off">
-                        @csrf
+    <div class="container">
+        <div class="row mt-3 mx-0">
+            <div class="col-md-6 light-block p-4 p-sm-5 p-md-4 p-lg-5">
+                <form class="form-signin" method="POST" action="{{ route('login') }}" autocomplete="off">
+                    @csrf
 
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                    <a class="navbar-brand mb-4" href="#">
+                        <img src="images/swace-logo-color-white-bg.svg" alt="">
+                    </a>
 
-                            <div class="col-md-6">
-                                <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}">
+                    <div class="text-left mt-5 mb-3">
+                        <h1 class="">Sign in</h1>
+                    </div>
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="form-label-group">
+                        <input type="email" id="email" class="form-control my-2 py-3 px-3{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Email address" required autofocus>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password">
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        @if($fail_counter > 2)
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="g-recaptcha form-control{{ $errors->has('recaptcha') ? ' is-invalid' : '' }}" data-sitekey="{{ $recaptcha }}"></div>
-                                @if ($errors->has('recaptcha'))
-                                    <span class="invalid-feedback">
-                                        <strong>{{ $errors->first('recaptcha') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                        @if ($errors->has('email'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
                         @endif
+                    </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
+                    <div class="form-label-group">
+                        <input type="password" id="password"  class="form-control my-2 py-3 px-3{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" placeholder="Password" required>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('password'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+
+                    @if($fail_counter > 2)
+                    <div class="form-label-group">
+                        <div class="g-recaptcha form-control{{ $errors->has('recaptcha') ? ' is-invalid' : '' }}" data-sitekey="{{ $recaptcha }}"></div>
+
+                        @if ($errors->has('recaptcha'))
+                            <span class="invalid-feedback">
+                                <strong>{{ $errors->first('recaptcha') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                    @endif
+
+                    <button class="btn btn-primary btn-block text-uppercase mt-3 p-3 mb-4" type="submit">Sign in</button>
+
+                    <div class="text-right muted-link">
+                        <a class="btn-link d-inline-block text-right py-1" href="{{ route('password.request') }}">Forgot password?</a>
+                    </div>
+                </form>
+            </div>
+            <div class="col-md-6 text-center text-md-right">
+                <div class="content">
+                        <span class="d-block d-md-inline-block py-3 mr-md-4">Don't have an account?</span>
+                        <a class="btn  btn-dark text-uppercase p-3 px-4" href="{{ route('register')}}">Sign up</a>
                 </div>
             </div>
+
         </div>
     </div>
-</div>
+</main>
 @endsection
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <meta property="og:site_name" content="Swace" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Swace" />
+    <meta property="og:image" content="{{ asset('/images/swacoin.svg') }}" />
+    <meta property="og:description" content="Project description" />
+    <meta property="og:url" content="{{ url()->current() }}" />
+    <link rel="icon" href="favicon.ico">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Google Tag Manager -->
+
+    <!-- End Google Tag Manager -->
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
+    @yield('head')
+</head>
+<body>
+
+    @if(Session::has('message'))
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="alert {!! Session::get('type') == 'success' ? 'alert-success' : 'alert-danger' !!}" role="alert">
+                    {{ Session::get("message") }}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endif
+
+    @yield('content')
+
+    <footer>
+        <div class="container">
+            <div class="copyright text-center py-5">&copy; 2016-2018 Swace Ltd.</div>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    
+</body>
+</html>
