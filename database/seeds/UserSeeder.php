f219c6b91f7800b3727f3a776ad1653193b51a31
vagrant <vagrant@homestead>
2018-05-09 06:38:13 +0000

    user seed admin if env is not local

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -13,19 +13,35 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
+        if (env('ENV') == 'local') {
+            $faker = Faker::create();
 
-        foreach (range(1, 100) as $x) {
+            foreach (range(1, 100) as $x) {
+                $user = new User;
+                $user->first_name = $faker->firstName;
+                $user->last_name = $faker->lastName;
+                $user->phone = $faker->phoneNumber;
+                $user->email = $faker->email;
+                $user->password = \Hash::make('admin');
+                $user->verified = rand(0, 1);
+                $user->contributed = rand(0, 1);
+                $user->created_at = '2016-05-01';
+                $user->verified_at = '2016-05-01';
+                $user->save();
+            }
+        } else {
             $user = new User;
-            $user->first_name = $faker->firstName;
-            $user->last_name = $faker->lastName;
-            $user->phone = $faker->phoneNumber;
-            $user->email = $faker->email;
-            $user->password = \Hash::make('admin');
-            $user->verified = rand(0, 1);
-            $user->contributed = rand(0, 1);
-            $user->created_at = '2016-05-01';
-            $user->verified_at = '2016-05-01';
+            $user->first_name = 'Administrator';
+            $user->last_name = '';
+            $user->phone = '00000';
+            $user->email = 'mosescaterpt@swace.io';
+            $user->password = \Hash::make('UitARTIBiTEGINiGnoBI');
+            $user->verified = 1;
+            $user->contributed = 1;
+            $user->admin = 1;
+            $user->kyc = 1;
+            $user->created_at = '1991-05-01';
+            $user->verified_at = '1991-05-01';
             $user->save();
         }
     }
