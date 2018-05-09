32d3da9c6a571aefc87973d29a0bef2bde5d4738
vagrant <vagrant@homestead>
2018-05-09 17:16:32 +0000

    RegisterController fixed: L63

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone');
-            $table->string('email', 254)->unique();
+            $table->string('email', 255)->unique();
             $table->string('password');
             $table->tinyInteger('verified')->default(0);
             $table->tinyInteger('contributed')->default(0);
