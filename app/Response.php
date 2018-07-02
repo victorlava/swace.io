69c6d9a28ede66450360842eac24171ebe0d87f8
Victor Lava <hello@victorlava.com>
2018-07-02 16:38:50 +0300

    added response fillable

--- app/Response.php
+++ app/Response.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Response extends Model
 {
-    //
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+  protected $fillable = [
+    'response', 'order_id', 'coingate_id'
+  ];
+
 }
