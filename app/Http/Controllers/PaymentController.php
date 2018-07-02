0541d19992ff1249691e2318e81fac5de910ce20
Victor Lava <hello@victorlava.com>
2018-07-02 15:16:53 +0300

    response remove

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -106,15 +106,14 @@ class PaymentController extends Controller
 
         if ($order) {
 
+            // $response = new App\Response();
+            // $response->create(['coingate_id' => $request->id,
+            //                    'order_id' => $request->order_id,
+            //                    'response' => $request->response]);
 
-            $order->paid(['request' => $request,
+            $order->paid(['request' => $request, 
                           'token_price' => $this->tokenPrice,
                           'bonus' => $this->bonusPercentage]);
-
-          $response = new App\Response();
-          $response->create([ 'coingate_id' => $request->id,
-                              'order_id' => $request->order_id,
-                              'response' => $request->response]);
         }
 
         return true;
