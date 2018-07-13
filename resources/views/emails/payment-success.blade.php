818d9473603981b2f532a1c46894198183bae060
Deivydas <deivydas.jurciukonis@gmail.com>
2018-07-13 11:23:46 +0300

    Email: Payment completed successfully! Hardcode 'swa.swace.io' changed to {{ env("MAIL_APP_URL") }}. Do not forget to add MAIL_APP_URL into .env

--- resources/views/emails/payment-success.blade.php
+++ resources/views/emails/payment-success.blade.php
@@ -9,9 +9,9 @@
 @endsection
 
 @section('content')
-    Your payment has been completed successfully.
-    Thank you for your payment. It has been processed and received by our partner CoinGate.<br/>
-    <br/>
-    You can view the number of purchased SWA tokens and your overall balance by logging into your dashboard at <a
-            href="{{ route('dashboard.index') }}">swa.swace.io</a>.
+    Your payment has been completed successfully.<br />
+
+    Thank you for your payment. It has been processed and received by our partner CoinGate.<br/><br/>
+
+    You can view the number of purchased SWA tokens and your overall balance by logging into your dashboard at <a href="{{ route('dashboard.index') }}">{{ env("MAIL_APP_URL") }}</a>.
 @endsection
